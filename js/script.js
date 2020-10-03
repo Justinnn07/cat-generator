052fb1909749e9cd6fb565df39ad0a7b0e4820a4
Justin <laptopdell390@gmail.com>
2020-10-03 09:33:25 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function GenerateCat () {
+    let image = document.createElement('img');
+    let div = document.getElementById('flex-cat');
+    image.src = "https://media1.giphy.com/media/13CoXDiaCcCoyk/200.webp?cid=ecf05e47okwfoc6dtpswj22de3ucw2u9pi6g3s6ovqdfci6z&rid=200.webp"
+    div.appendChild(image);
+}
